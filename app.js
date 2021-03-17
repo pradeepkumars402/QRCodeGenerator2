60c7382f655c11f7187c1c8270ee95b0a027e08c
psakalabhaktula <psakalabhaktula@in.imshealth.com>
2021-03-17 15:28:33 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const port=process.env.PORT || 3001
+const express = require('express')
+const app = express()
+const QRCode = require('qrcode')
+ 
+app.get('/sourceURL=:sourceURL&width=:width&height=:height', function (req, res) {
+    var QRoptions= {
+        errorCorrectionLevel : 'H',
+        type : 'image/jpeg',
+        quality: 0.3, 
+        margin: 1,
+        color: {
+            dark:"#010599FF",
+            light:"#FFBF60FF"
+          },
+          width : req.params.width,
+          height : req.params.height
+    }
+    QRCode.toDataURL(req.params.sourceURL,QRoptions, function (err, url) {
+        res.send(url);
+    })
+})
+ 
+app.listen(port)
